a3666a671e089afb235310b5ddbb63d1d801c6cf
NCPreacher <bachuszmarcin7@gmil.com>
2020-04-02 00:09:11 +0200

    Prototyp_menu
    -Stworzono prototyp menu
    -Stworzono prototyp stylu przewodniego aplikacji

---
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addMeal() {}
+    public void showStatistics() {}
+    public void openOptions() {}
+    public void quitApplication() {}
+
+}
